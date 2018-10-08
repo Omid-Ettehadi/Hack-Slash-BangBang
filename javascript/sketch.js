4a2dce132d1e29b337785954a1fa83694aa040c0
Omid <omid_ettehadi@yahoo.com>
2018-10-08 10:33:03 -0400

    Added Functionality to the Character Buttons
    Preloaded all the images for tools and assigned the Tools variable based on the character chosen

--- javascript/sketch.js
+++ javascript/sketch.js
@@ -1,14 +1,35 @@
 var canvas;
 var x, y;
 
+var hButton, zButton, cButton, sButton;
 var buttonSize,buttonMargin;
-var hButton, zButton;
+
+var hasGameStarted;
+var isCharacterChosen;
+
+var hTools = [];
+var zTools = [];
+var Tools = [];
+
 
 function preload() {
-	
+	// Preload all the tools
+	// Human Tools
+	for (var i = 0; i < 3; i++){
+        hTools [i] = loadImage("images/hTools/htool" + i + ".png");
+    }
+	// Zombie Tools
+	for (var i = 0; i < 3; i++){
+        zTools [i] = loadImage("images/zTools/ztool" + i + ".png");
+    }
 }
 
 function setup() {
+	// Initializing
+	cButton = 0; // cButton is not created yet
+    sButton = 0; // sButton is not created yet
+    isCharacterChosen = 0; // Character not chosen
+	
 	// Canvas
 	x = window.innerWidth;
 	y = window.innerHeight;
@@ -26,20 +47,66 @@ function setup() {
         
     // Buttons
 	// Human Button
-    hButton = createImg('images/human.png');
+    hButton = createImg('images/icons/human.png');
     hButton.size(buttonSize - 10, buttonSize - 10);
     hButton.position((x/4)-75, y - buttonSize + buttonMargin);
+	hButton.mousePressed(hFunction);
 
 	// Zombie Button	
-    zButton = createImg('images/zombie.png');
+    zButton = createImg('images/icons/zombie.png');
     zButton.size(buttonSize - 20, buttonSize - 20);
     zButton.position((3*x/4)-75, y - buttonSize + buttonMargin);
+	zButton.mousePressed(zFunction);
 }
 
 function draw() {
 	// Do nothing
 }
 
+function gFuntion() {
+	// Create shake button
+    sButton = createImg('images/shake.png');
+    sButton.size(250, 150);
+    sButton.position((x/2) - 125, ((y - buttonSize)/2) - 75);
+}
+
+function hFunction() {
+	// If character is not chosen, assign human tools to the Tools variable
+    if (isCharacterChosen == 0) {
+        Tools = hTools;
+		
+		// Create cButton
+        cButton = createImg('images/cards/hCard1.png');
+        cButton.size(309, 500);
+        cButton.position((x/2) - 155, ((y - buttonSize)/2) - 250);
+        cButton.mousePressed(gFuntion);
+        
+		// Set character as chosen (human)
+        isCharacterChosen = 1;
+    } 
+}
+
+function zFunction() {
+	// If character is not chosen, assign zombie tools to the Tools variable
+    if (isCharacterChosen == 0){
+        Tools = zTools;
+		
+		// Create cButton
+        cButton = createImg('images/cards/hCard1.png');
+        cButton.size(309, 500);
+        cButton.position((x/2) - 155, ((y - buttonSize)/2) - 250);
+        cButton.mousePressed(gFuntion);
+        
+		// Set character as chosen (zombie)
+        isCharacterChosen = 2;
+    }
+}
+
 function windowResized() {
-    setup();   
+    if ( cButton != 0){
+        cButton.remove();
+    }
+    hButton.remove();
+    zButton.remove();
+    setup();
 }
